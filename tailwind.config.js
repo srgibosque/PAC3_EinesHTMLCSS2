df07e1e57a0d0fa28db74fc3411f0ac5f6dd03f2
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2025-01-09 23:05:40 +0100

    structure and speakers page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      'white': '#ffffff',
+      'midnight-steel': '#1c3146',
+      'eclipse-night': '#07080d',
+      'primary-blue': '#ff49db',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+      mono: ['Inconsolata', 'monospace'],
+    },
     extend: {},
   },
   plugins: [],
