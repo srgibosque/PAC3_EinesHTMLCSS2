679c06d01e21ad55717e0bc1aabbc56340e80a30
[Sergi Bosque Ródenas] <[sbosquero@uoc.edu]>
2025-01-10 18:45:03 +0100

    Contact form page

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,17 @@ module.exports = {
       'white': '#ffffff',
       'midnight-steel': '#1c3146',
       'eclipse-night': '#07080d',
-      'primary-blue': '#ff49db',
+      'primary-blue': '#2e3cf4',
     },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
       mono: ['Inconsolata', 'monospace'],
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        'focus': '0 0 0 0.25rem rgba(46, 60, 244, 0.4)',
+      },
+    },
   },
   plugins: [],
 }
